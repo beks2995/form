e41067e25f99dc5327ea74160ef4f07d35ca4dfa
kapar <kapar@fromjimmy.com>
2022-03-03 14:58:00 +0600

    styles fixed

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,16 @@ export default App;
 
 /**
  *  configure tsconfig.json
+ *  ====1 step настройка стилей====
  *  - paths with lowercase
  *  - styled-component https://styled-components.com/
  *  - sass - install
+ *  - global.scss import in the App.tsx и чтобы стили начали работать
+ *  - styling by the style component
  * 
- * 
+ *   ====2 step настройка кода====
+ *  - noImplementic any - то есть когда я делаю any type ts должен не давать мне это делать
+ *  - eslint|tslint|prettier - любой стандарт ссылки найдешь сам
+ *  - precommit-hooks - https://www.npmjs.com/package/husky - через него должен работать pre-commit
+ *  - prepush-hook - тоже через husky
  */
